841caf69cb80e9b2741f6bbe25431fe1c5ddd5f8
simrankhinvasara <simrankhinvasara@gmail.com>
2020-09-16 11:34:59 +0000

    new helloworld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,13 @@
+// Simple Java Program to Print Hello World
 public class HelloWorld {
-    public static void main(String[] args) {
-	System.out.println("Hello, World from crazy-experiment branch");
 
-    }
+	public static void main(String[] args)
+	{
+		int i;
+		for(i = 0; i <= 5; i++)
+		{
+			System.out.println("\n Hello World ");
+		}	
+	}
 }
 
